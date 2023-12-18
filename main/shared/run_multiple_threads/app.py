3a8dc44bc4cfa4bcd316234ea643ee2607f59102
Olufemi Jemilohun <olufemi.jemilohun@gmail.com>
2023-12-18 05:39:00 -0500

    Adding logic for running tests in multiple threads

---
+++ main/shared/run_multiple_threads/app.py
@@ -0,0 +1,396 @@
+#!.venv/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import asyncio
+import dataclasses as dc
+import inspect
+import threading
+import time
+from types import ModuleType
+from typing import Any, Callable, List
+
+from error_handler.app import main as error_handler
+from get_config.app import main as get_config
+from utils import app as utils
+
+
+MODULE = __file__
+CONFIG = get_config(module=MODULE)
+LOCALS = locals()
+
+STORE = {}
+LOCK = threading.Lock()
+
+
+@dc.dataclass
+class Data_Class:
+  pass
+
+
+@error_handler()
+async def get_task_from_event_loop(
+  task: Any | None = None
+) -> Any:
+  condition = inspect.iscoroutine(task)
+  if condition is False:
+    return task
+
+  loop = asyncio.new_event_loop()
+  asyncio.set_event_loop(loop)
+
+  try:
+    task = loop.run_until_complete(task)
+  finally:
+    loop.close()
+    asyncio.set_event_loop(None)
+
+  return task
+
+
+@error_handler()
+async def format_argument_as_empty_list(
+  # trunk-ignore(ruff/ARG001)
+  argument: None = None,
+) -> list:
+  return []
+
+
+@error_handler()
+async def format_argument_as_argument(argument: dict | tuple | list) -> dict:
+  return argument
+
+
+@error_handler()
+async def format_argument_as_list(argument: Any) -> list:
+  return [argument]
+
+
+@error_handler()
+async def format_argument(
+  argument: Any | None = None,
+  kind: str | None = None,
+) -> list | dict:
+  _ = kind
+  kind = argument.__class__.__name__.lower()
+  kind = kind if kind in CONFIG.format_kinds else 'any'
+  kind = CONFIG.format_kinds_handler[kind]
+
+  handler = f'format_argument_as_{kind}'
+  handler = LOCALS[handler]
+  return handler(argument=argument)
+
+
+def update_store(result_key: str):
+  def decorator(function):
+    def entrypoint_wrapper(
+      # trunk-ignore(ruff/ARG001)
+      *args,
+      # trunk-ignore(ruff/ARG001)
+      **kwargs
+  ) -> Callable:
+      global STORE
+
+      result = function()
+
+      condition = result_key in STORE
+      if condition:
+        STORE[result_key].append(result)
+      elif not condition:
+        STORE[result_key] = [result]
+
+      return result
+    return entrypoint_wrapper
+  return decorator
+
+
+@error_handler()
+async def format_target_arguments(
+  kwargs: list | None = None,
+  args: tuple | list | None = None,
+) -> dict:
+  arguments = []
+
+  for name in ['args', 'kwargs']:
+    values = locals().get(name, None)
+    if not values:
+      continue
+
+    kind = type(values).__name__.lower()
+
+    conditions = [name == 'kwargs', kind == 'list']
+    if sum(conditions) == len(conditions):
+      for value in values:
+        arguments.append({name: value})
+    elif sum(conditions) != len(conditions):
+      arguments.append({name: values})
+
+  return {
+    'args': None,
+    'kwargs': None,
+    'arguments': arguments, }
+
+
+@error_handler()
+async def call_sync_target_with_packed_argument(
+  argument: Any | None = None,
+  target: Callable | None = None,
+  exception: Exception | None = None,
+  output: Any | None = None,
+) -> Any:
+  if output and not exception:
+    return output
+
+  try:
+    return target(argument)
+  except Exception as e:
+    _ = e
+    return exception
+
+
+@error_handler()
+async def call_sync_target_with_unpacked_argument(
+  argument: Any | None = None,
+  target: Callable | None = None,
+  kind: str | None = None,
+) -> dict:
+  output = None
+  exception = None
+
+  try:
+    if kind == 'kwargs':
+      output = target(**argument)
+    elif kind == 'args':
+      output = target(*argument)
+  except Exception as e:
+    exception = e
+
+  return {
+    'exception': exception,
+    'output': output, }
+
+
+@error_handler()
+async def call_async_target_with_packed_argument(
+  argument: Any | None = None,
+  target: Callable | None = None,
+  exception: Exception | None = None,
+  output: Any | None = None,
+) -> dict:
+  if exception is None:
+    return output
+
+  try:
+    task = target(argument)
+    return get_task_from_event_loop(task=task)
+  except Exception as e:
+    _ = e
+    return exception
+
+
+@error_handler()
+async def call_async_target_with_unpacked_argument(
+  argument: Any | None = None,
+  target: Callable | None = None,
+  kind: str | None = None,
+) -> dict:
+  output = None
+  exception = None
+
+  try:
+    if kind == 'kwargs':
+      output = target(**argument)
+    elif kind == 'args':
+      output = target(*argument)
+  except Exception as e:
+    exception = e
+
+  return {
+    'exception':  get_task_from_event_loop(task=exception),
+    'output':  get_task_from_event_loop(task=output), }
+
+
+@error_handler()
+async def get_entrypoint_for_sync_target(
+  target: Callable | None = None,
+  argument: dict | None = None,
+  result_key: str | None = None,
+  kind: str | None = None,
+) -> Callable:
+
+  @update_store(result_key=result_key)
+  def entrypoint() -> None:
+    data = call_sync_target_with_unpacked_argument(
+      target=target,
+      argument=argument,
+      kind=kind, )
+    return call_sync_target_with_packed_argument(
+      target=target,
+      exception=data.get('exception'),
+      output=data.get('output'),
+      argument=argument, )
+
+  return entrypoint
+
+
+@error_handler()
+async def get_entrypoint_for_async_target(
+  target: Callable | None = None,
+  argument: dict | None = None,
+  result_key: str | None = None,
+  kind: str | None = None,
+) -> Callable:
+
+  @update_store(result_key=result_key)
+  def entrypoint() -> dict:
+    data = call_async_target_with_unpacked_argument(
+      target=target,
+      argument=argument,
+      kind=kind, )
+    return call_async_target_with_packed_argument(
+      target=target,
+      exception=data.get('exception'),
+      output=data.get('output'),
+      argument=argument, )
+
+  return entrypoint
+
+
+@error_handler()
+async def get_entrypoint_handler(
+  target: Callable,
+  argument: dict | tuple| list | None = None,
+  result_key: str | None = None,
+) -> Callable:
+  kind = 'args'
+
+  key = 'kwargs'
+  if key in argument:
+    kind = 'kwargs'
+  argument = argument[kind]
+
+  coroutine = 'async' if inspect.iscoroutinefunction(target) else 'sync'
+  handler = f'get_entrypoint_for_{coroutine}_target'
+  handler = LOCALS[handler]
+  return handler(
+    target=target,
+    argument=argument,
+    result_key=result_key,
+    kind=kind, )
+
+
+@error_handler()
+async def set_target_entrypoints_for_threads(
+  target: Callable | None = None,
+  arguments: list | None = None,
+  result_key: str | None = None,
+  module: ModuleType | None = None,
+) -> dict:
+  entrypoints = []
+
+  for argument in arguments:
+    entrypoint = get_entrypoint_handler(
+      target=target,
+      result_key=result_key,
+      argument=argument, )
+    entrypoints.append(entrypoint)
+
+  kind = type(module).__name__.lower()
+  if kind == 'module':
+      module = inspect.getmodule(target)
+  elif kind == 'list':
+    module = module[0]
+  elif kind == 'nonetype':
+    module = ''
+
+  return {
+    'module': module,
+    'entrypoints': entrypoints,
+    'arguments': None, }
+
+
+@error_handler()
+async def create_single_thread(
+  target: Callable | None = None,
+  daemon: bool | None = True,
+) -> threading.Thread:
+  return threading.Thread(target=target, daemon=daemon)
+
+
+@error_handler()
+async def get_threads(entrypoints: list | None = None) -> dict:
+  threads = []
+
+  for entrypoint in entrypoints:
+    thread = create_single_thread(target=entrypoint)
+    # Lock to prevent duplicate threads from being created
+    with LOCK:
+      time.sleep(0.1)
+    threads.append(thread)
+
+  return {
+    'entrypoints': None,
+    'threads': threads, }
+
+
+@error_handler()
+async def run_threads_in_parallel(
+  threads: threading.Thread | None = None,
+) -> dict:
+  threads = [] if not threads else threads
+  n = range(len(threads))
+
+  for i in n:
+    threads[i].start()
+  for i in n:
+    threads[i].join()
+
+  return {'threads': None}
+
+
+@error_handler()
+async def run_threads_in_sequence(
+  threads: threading.Thread | None = None,
+) -> dict:
+  threads = [] if not threads else threads
+  n = range(len(threads))
+
+  for i in n:
+    threads[i].start()
+    threads[i].join()
+
+  return {'threads': None}
+
+
+@error_handler()
+async def main(
+  target: List[Callable] | Callable | None = None,
+  kwargs: List[dict] | dict | None = None,
+  args: List[Any] | None = None,
+  timestamp: int | None = None,
+) -> List[Any]:
+  args = () if not args else args
+  kwargs = {} if not kwargs else kwargs
+  timestamp = int(time.time()) if not timestamp else timestamp
+  result_key = f'{target.__name__}.{timestamp}'
+
+  data = utils.process_arguments(
+    data_class=CONFIG.schema.Data,
+    locals=locals(), )
+  data = utils.process_operations(
+    operations=CONFIG.operations,
+    functions=LOCALS,
+    data=data, )
+
+  return STORE.get(result_key, None)
+
+
+def example() -> None:
+  from invoke_pytest.app import main as invoke_pytest
+
+
+  invoke_pytest(project_directory=MODULE)
+
+
+if __name__ == '__main__':
+  example()
---
+++ main/shared/run_multiple_threads/test_resources/app.py
@@ -0,0 +1,182 @@
+import asyncio
+import dataclasses as dc
+import threading
+from time import ctime, time
+from typing import Callable
+import threading
+from typing import Any
+import inspect
+import os
+
+import yaml
+
+
+MODULE = __file__
+PARENT_MODULE = os.path.dirname(os.path.dirname(MODULE)) + f'{os.sep}app.py'
+
+LOCALS = locals()
+# Global variable to store results from
+# executing target functions in threads
+THREAD_EXECUTION_RESULTS = []
+
+TYPE_HANDLER = {
+  "str": lambda name: f"Hello {name}",
+  "NoneType": lambda name: "Hello World", }
+
+
+def main_sync(name: str = None) -> str:
+  kind = type(name).__name__.lower()
+  if kind not in TYPE_HANDLER:
+    kind = 'NoneType'
+  handler = TYPE_HANDLER[kind]
+  result = handler(name=name)
+  return result
+
+
+async def main_async(name: str | None = None) -> str:
+  kind = type(name).__name__.lower()
+  if kind not in TYPE_HANDLER:
+    kind = 'NoneType'
+  handler = TYPE_HANDLER[kind]
+  result = handler(name=name)
+  return result
+
+
+def add_sync(
+  a: int = 0,
+  b: int = 0,
+) -> int:
+  return a + b
+
+
+async def add_async(
+  a: int = 0,
+  b: int = 0,
+) -> int:
+  return a + b
+
+
+def sync_target(*args, **kwargs) -> str:
+  return 'sync_output'
+
+
+async def async_target(*args, **kwargs) -> str:
+  return 'async_output'
+
+
+def argument_example(argument) -> tuple | None:
+  if argument == 'tuple':
+    return ()
+
+
+def sync_entrypoint(*args, **kwargs) -> str:
+  return 'sync_entrypoint_output'
+
+
+async def async_entrypoint(*args, **kwargs) -> str:
+  return 'async_entrypoint_output'
+
+
+def function_resource(function: str) -> Callable:
+  return LOCALS[function]
+
+
+def entrypoints_resource(entrypoints: str) -> list:
+  if entrypoints == 'entrypoints':
+    return [sync_target, async_target]
+  
+  return entrypoints
+
+
+def argument_example_1(argument) -> dict:
+  key = 'entrypoint'
+  entrypoint = entrypoint_example(entrypoint=argument[key])
+  argument[key] = entrypoint
+  return argument
+
+
+def threads_resource(threads: str | None) -> list:
+  if threads == 'threads':
+    return [
+      threading.Thread(target=add_sync),
+      threading.Thread(target=add_async), ]
+
+
+def sync_exception_target(*args, **kwargs):
+  return sum([1, '1'])
+
+
+async def async_exception_target(*args, **kwargs):
+  return sum([1, '1'])
+
+
+def target_resource(target: str | dict) -> Callable:
+  kind = target.__class__.__name__.lower()
+
+  if kind == 'dict':
+    target = target.get('target')
+
+  if kind == 'function':
+    return target
+
+  if isinstance(target, str):
+    return LOCALS.get(target, None)
+  
+  return target
+
+
+def get_threads_resource(threads: list | None = None) -> list:
+  threads = [] if not threads else threads
+  store = []
+  for thread in threads:
+    kind = type(thread).__name__.lower()
+    store.append(kind)
+  return store
+
+
+def call_entrypoint(entrypoint: Callable) -> Any:
+  result = entrypoint()
+  
+  condition = inspect.iscoroutine(result)
+  if condition:
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(result)
+  
+  return result
+
+
+def coroutine_resource(*args, **kwargs) -> Any:
+  async def async_function():
+    return 'async_output'
+  return async_function()
+
+
+def set_target_entrypoints_for_threads_resource(entrypoints: list) -> list:
+  store = []
+  for entrypoint in entrypoints:
+    store.append(entrypoint.__name__)
+  return store
+
+
+def output_threads_resource(threads: list | None) -> list:
+  threads = threads or []
+
+  store = []
+  for thread in threads:
+    data = {
+      'target': thread._target.__name__,
+      'type': type(thread).__name__.lower(), }
+    store.append(data)
+
+  return store
+
+
+def example() -> None:
+  from invoke_pytest.app import main as invoke_pytest
+
+
+  invoke_pytest(project_directory=PARENT_MODULE)
+
+
+if __name__ == '__main__':
+  example()
