59a5dd0583968d22330642c89593987f69d18009
Olufemi Jemilohun <olufemi.jemilohun@gmail.com>
2024-05-19 01:38:08 -0400

    Updating how tests processed through app are retrieved

--- test_entrypoint.py
+++ test_entrypoint.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+from types import ModuleType
 from types import SimpleNamespace as sns
 
 import pytest
@@ -12,10 +13,14 @@ def get_test_id(val) -> str:
     return val.id
 
 
+def get_yaml_tests(pytest: ModuleType) -> list:
+  return getattr(pytest, 'yaml_tests', None) or []
+
+
 @pytest.mark.parametrize(
   argnames='test',
   ids=get_test_id,
-  argvalues=pytest.yaml_tests, )
+  argvalues=get_yaml_tests(pytest=pytest), )
 def test_(test: sns) -> None:
   # trunk-ignore(bandit/B101)
   assert test.expected == test.output
